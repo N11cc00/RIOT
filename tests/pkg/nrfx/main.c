64452faa3e8fafb1e233536d39cfb2a613e90790
KSKNico <nifrabe@outlook.de>
2024-09-25 17:02:54 +0200

    test requires newer version of nrfx, nrfx was updated to version 3.6.0

--- tests/pkg/nrfx/main.c
+++ tests/pkg/nrfx/main.c
@@ -1,5 +1,17 @@
 #include <nrfx_nfct.h>
+#include <assert.h>
+
+static void test_init(void) {
+    nrfx_nfct_config_t config = {
+        .rxtx_int_mask = 0,
+        .cb = NULL,
+        .irq_priority = 0
+    };
+    nrfx_err_t error = nrfx_nfct_init(&config);
+    assert(error == NRFX_SUCCESS);
+}
 
 int main(void) {
+    test_init();
     return 0;
 }
