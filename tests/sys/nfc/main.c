126e92d2847b8e113ab87311c15b050e5b607481
BarschusRochus <19max60@web.de>
2024-12-04 18:10:06 +0100

    added test for t2t functionality

---
+++ tests/sys/nfc/main.c
@@ -0,0 +1,18 @@
+//#include "../../../sys/include/net/nfc/t2t/t2t.h"
+#include "net/nfc/t2t/t2t.h"
+#include <stddef.h>
+#include <stdio.h>
+
+int main(void){
+    uint8_t mem[NFC_T2T_STATIC_MEMORY_SIZE];
+    //uint8_t ndef_message_content[] = {'H','e','l','l','o',' ','W','o','r','l','d'};
+    nfc_t2t_t tag = create_type_2_tag(NULL, NULL, NULL, NFC_T2T_STATIC_MEMORY_SIZE, mem);
+    printf("Tag created\n");
+    uint8_t buf[16];
+    t2t_read_block(&tag, 0,buf);
+    printf("read block\n");
+    for(int i=0; i<16;i++){
+        printf("Block 0: %#04X\n", buf[i]);
+    }
+}
+
