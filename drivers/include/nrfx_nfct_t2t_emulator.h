eccfd23532e97feeeddfccd6fc90e311f13010c7
KSKNico <nifrabe@outlook.de>
2025-01-15 18:35:10 +0100

    added documentation to the nrfx t2t emulator

--- drivers/include/nrfx_nfct_t2t_emulator.h
+++ drivers/include/nrfx_nfct_t2t_emulator.h
@@ -15,7 +15,7 @@
 
 typedef struct {
     event_t super;
-    uint32_t size; 
+    uint32_t size;
 } nrfx_t2t_receive_event_t;
 
 typedef struct {
@@ -23,13 +23,32 @@ typedef struct {
     uint32_t size;
 } nrfx_t2t_transmit_event_t;
 
-
+/**
+ * @brief Initializes the Type 2 Tag emulator
+ *
+ * @note The tag will be modified and read after passing it to the emulator. To
+ * guarantee that the emulator works correctly, the tag after initialization.
+ * The tag can be enabled after initialization.
+ *
+ * @param[in] tag Tag to use for the emulator
+ */
 void initialize_t2t(nfc_t2t_t* tag);
 
+/**
+ * @brief Unititializes the Type 2 Tag emulator
+ *
+ * @note This only works on a tag that is not enabled.
+ */
 void uninitialize_t2t(void);
 
+/**
+ * @brief Enables an initialized Type 2 Tag emulator
+ */
 void enable_t2t(void);
 
+/**
+ * @brief Disables an enabled Type 2 Tag emulator
+ */
 void disable_t2t(void);
 
 #endif
--- drivers/nrfx_nfct_t2t_emulator/nrfx_nfct_t2t_emulator.c
+++ drivers/nrfx_nfct_t2t_emulator/nrfx_nfct_t2t_emulator.c
@@ -23,21 +23,26 @@ static uint8_t data_buffer_rx[BUFFER_SIZE];
 static uint8_t data_buffer_tx[BUFFER_SIZE];
 static const uint32_t buffer_size = BUFFER_SIZE;
 
-// static nrfx_nfct_data_desc_t data_desc_tx;
-// static nrfx_nfct_data_desc_t data_desc_rx;
-
 static nrfx_t2t_receive_event_t receive_event;
 static event_t disable_event;
 static event_t enable_event;
 static event_t disable_event;
 static event_t enable_event;
 static event_t uninitialize_event;
-// static t2t_transmit_event_t transmit_event;
 static event_t select_event;
 static event_t end_of_transmission_event;
 static event_t field_detected_event;
 static event_t start_of_rx_event;
 
+/**
+ * @brief Enables autocollision resolution in the NFCT peripheral
+ *
+ * @note Without autocollision resolution, the user would have to implement
+ * their own autocollision procedure
+ *
+ * @param[in] nfctid1       Pointer to the NFCID1 to use
+ * @param[in] nfctid1_size  Size of the NFCID1
+ */
 static void configure_autocolres(uint8_t *nfctid1, uint8_t nfctid1_size) {
     nrfx_nfct_nfcid1_t nfcid1 = {
         .p_id = nfctid1,
@@ -52,6 +57,12 @@ static void configure_autocolres(uint8_t *nfctid1, uint8_t nfctid1_size) {
     nrfx_nfct_autocolres_enable();
 }
 
+/**
+ * @brief Helper function that prints bytes in hex format
+ *
+ * @param[in] bytes Bytes to print
+ * @param[in] size  Size of the bytes
+ */
 static void print_bytes_as_hex(const uint8_t* bytes, uint32_t size) {
     for (uint32_t i = 0; i < size; i++) {
         printf("%02X ", bytes[i]);
@@ -59,6 +70,12 @@ static void print_bytes_as_hex(const uint8_t* bytes, uint32_t size) {
     printf("\n");
 }
 
+
+/**
+ * @brief Sends ack or nack response
+ *
+ * @param[in] ack sends ack if true, nack if false
+ */
 static void send_ack_nack(bool ack) {
     uint8_t buffer[1];
     if (ack) {
@@ -76,21 +93,43 @@ static void send_ack_nack(bool ack) {
     nrfx_nfct_tx(&data_description, NRF_NFCT_FRAME_DELAY_MODE_WINDOWGRID);
 }
 
-static void process_write_command(uint8_t block_no, uint8_t const *bytes) {
-    
-    // write 4 bytes to the address
 
-    // print_bytes_as_hex(bytes, 4);
+/**
+ * @brief Process a write command
+ *
+ * @param block_no  Block number to write to
+ * @param bytes     4 bytes of data to write
+ */
+static void process_write_command(uint8_t block_no, uint8_t const *bytes) {
+
+    /* write 4 bytes to the address */
     if (!t2t_handle_write(tag, block_no, bytes)) {
         send_ack_nack(false);
+        return;
+    }
+
+    /* if the tag is not writeable, return a NACK */
+    if (!t2t_is_writeable(tag)) {
+        LOG_WARNING("Tag is not writeable\n");
+        send_ack_nack(false);
+        return;
     }
 
     LOG_DEBUG("Wrote 4 bytes to block number %d", block_no);
     send_ack_nack(true);
     print_bytes_as_hex(tag->memory, tag->memory_size);
-    
+
 }
 
+/**
+ * @brief Event loop for the NRFX T2T emulator
+ *
+ * @note This should run in a separate thread as it pulls events from the event queue with a
+ * blocking call to event_wait.
+ *
+ * @param[in] arg Argument of the thread
+ * @return void* Returns nothing
+ */
 static void* nrfx_event_loop(void *arg) {
     (void) arg;
     event_queue_init(&event_queue);
@@ -106,22 +145,34 @@ static void* nrfx_event_loop(void *arg) {
 
 
 
-// send the data at the given block address
-// always sends 16 bytes
-static void process_read_command(uint8_t block_address) {
-    t2t_handle_read(tag, block_address, data_buffer_tx);
+/**
+ * @brief Processes a read command
+ *
+ * @param[in] block_no block number to read from
+ */
+static void process_read_command(uint8_t block_no) {
+    t2t_handle_read(tag, block_no, data_buffer_tx);
 
-    // Set up the data descriptor for the response
+    /* Set up the data descriptor for the response */
     nrfx_nfct_data_desc_t data_desc_tx = {
         .p_data = data_buffer_tx,
         .data_size = 16
     };
 
-    // Transmit the response
+    /* Transmit the response */
     nrfx_err_t error = nrfx_nfct_tx(&data_desc_tx, NRF_NFCT_FRAME_DELAY_MODE_WINDOWGRID);
     assert(error == NRFX_SUCCESS);
-} 
+}
 
+
+/**
+ * @brief Process received data
+ *
+ * @note Depending on the content either a read, write or halt command is processed
+ *
+ * @param[in] data Data to process
+ * @param[in] size Size of the data
+ */
 static void parse_received_data(const uint8_t *data, uint32_t size) {
     if (size == 0) {
         LOG_DEBUG("Received empty data\n");
@@ -141,15 +192,15 @@ static void parse_received_data(const uint8_t *data, uint32_t size) {
         process_read_command(block_address);
     } else if (command == T2T_HALT_COMMAND) {
         LOG_DEBUG("Halt command received\n");
-        // TODO: work out redundant scanning of tag
-        // nrf_nfct_shorts_disable(NRF_NFCT, NRF_NFCT_SHORT_FIELDDETECTED_ACTIVATE_MASK);
-        // nrfx_nfct_init_substate_force(NRFX_NFCT_ACTIVE_STATE_SLEEP);
+        /* TODO: work out redundant scanning of tag
+        nrf_nfct_shorts_disable(NRF_NFCT, NRF_NFCT_SHORT_FIELDDETECTED_ACTIVATE_MASK);
+        nrfx_nfct_init_substate_force(NRFX_NFCT_ACTIVE_STATE_SLEEP); */
         selected = false;
     } else if (command == T2T_WRITE_COMMAND) {
         if (data_size != 5) {
             LOG_WARNING("Write command does not contain the correct amount of bytes");
             return;
-        } 
+        }
         process_write_command(*data_buffer, data_buffer + 1);
     } else if (command == T2T_SECTOR_SELECT_COMMAND) {
         send_ack_nack(false);
@@ -159,11 +210,21 @@ static void parse_received_data(const uint8_t *data, uint32_t size) {
     }
 }
 
+/**
+ * @brief Handles a receive event
+ *
+ * @param[in] event Receive event to process
+ */
 static void receive_handler(event_t * event) {
     nrfx_t2t_receive_event_t *e = container_of(event, nrfx_t2t_receive_event_t, super);
     parse_received_data(data_buffer_rx, e->size);
 }
 
+/**
+ * @brief Handles an enable event
+ *
+ * @param[in] event Enable event to process
+ */
 static void enable_handler(event_t *event) {
     (void) event;
     LOG_DEBUG("[EVENT] Enabling T2T\n");
@@ -172,6 +233,11 @@ static void enable_handler(event_t *event) {
     enabled = true;
 }
 
+/**
+ * @brief Handles a disable event
+ *
+ * @param[in] event Disable event to process
+ */
 static void disable_handler(event_t *event) {
     (void) event;
 
@@ -179,25 +245,23 @@ static void disable_handler(event_t *event) {
     enabled = false;
 }
 
+/**
+ * @brief Handles an uninitialize event
+ *
+ * @param[in] event Uninitialize event to process
+ */
 static void uninitialize_handler(event_t *event) {
     (void) event;
 
     nrfx_nfct_uninit();
     thread_zombify();
 }
-/*
-static void transmit_handler(event_t * event) {
-    t2t_transmit_event_t *e = container_of(event, t2t_transmit_event_t, super);
-    memset(data_buffer_tx, 0, buffer_size);
-    nrfx_nfct_data_desc_t data_desc_tx = {
-        .p_data = data_buffer_tx,
-        .data_size = e->size
-    };
 
-    nrfx_err_t error = nrfx_nfct_tx(&data_desc_tx, NRF_NFCT_FRAME_DELAY_MODE_WINDOWGRID);
-}
-*/
-
+/**
+ * @brief Handles the end of a transmission event
+ *
+ * @param[in] event End of transmission event to process
+ */
 static void end_of_tx_handler(event_t * event) {
     (void) event;
     memset(data_buffer_rx, 0, buffer_size);
@@ -208,12 +272,22 @@ static void end_of_tx_handler(event_t * event) {
     nrfx_nfct_rx(&data_desc_rx);
 }
 
+/**
+ * @brief Handles the start of a receive event
+ *
+ * @param[in] event Start of receive event to process
+ */
 static void start_of_rx_handler(event_t * event) {
     (void) event;
     LOG_DEBUG("Receiving data\n");
 }
 
-static void selected_handler(event_t * event) {
+/**
+ * @brief Handles a select event
+ *
+ * @param[in] event Select event to process
+ */
+static void select_handler(event_t * event) {
     (void) event;
     memset(data_buffer_rx, 0, buffer_size);
     nrfx_nfct_data_desc_t data_desc_rx = {
@@ -224,39 +298,20 @@ static void selected_handler(event_t * event) {
     nrfx_nfct_rx(&data_desc_rx);
 }
 
+/**
+ * @brief Field detected event handler
+ *
+ * @param[in] event Field detected event to process
+ */
 static void field_detected_handler(event_t * event) {
     (void) event;
 
     LOG_DEBUG("[EVENT] Field detected\n");
-    // nrf_nfct_shorts_enable(NRF_NFCT, NRF_NFCT_SHORT_FIELDDETECTED_ACTIVATE_MASK);
+    /* nrf_nfct_shorts_enable(NRF_NFCT, NRF_NFCT_SHORT_FIELDDETECTED_ACTIVATE_MASK); */
 }
 
 /*
-static void field_lost_handler(event_t * event) {
-    LOG_DEBUG("[EVENT] Field lost\n");
-    selected = false;
-}
-*/
-
-/* user_write to tag {
-post_event_queue(event_queue, write_event);
-}
-
-write_handler(event) {
-    writeBlock();
-}
-
-*/
-
-void print_state(nrf_nfct_tag_state_t state) {
-    /*
-    NRF_NFCT_TAG_STATE_DISABLED 	
-    NRF_NFCT_TAG_STATE_RAMP_UP 	
-    NRF_NFCT_TAG_STATE_IDLE 	
-    NRF_NFCT_TAG_STATE_RECEIVE 	
-    NRF_NFCT_TAG_STATE_FRAME_DELAY 	
-    NRF_NFCT_TAG_STATE_TRANSMIT 	
-    */
+static void print_state(nrf_nfct_tag_state_t state) {
 
    if (state == NRF_NFCT_TAG_STATE_DISABLED) {
        LOG_DEBUG("Tag is disabled\n");
@@ -272,23 +327,27 @@ void print_state(nrf_nfct_tag_state_t state) {
        LOG_DEBUG("Tag is transmitting data\n");
    }
 }
+*/
 
-// this is the irq callback function that is called by the nfct driver
-void irq_event_handler(nrfx_nfct_evt_t const * event) {
-    // print_state(nrf_nfct_tag_state_get(NRF_NFCT));
+/**
+ * @brief IRQ event handler called by the NRFX NFCT driver
+ *
+ * @param[in] event event to process
+ */
+static void irq_event_handler(nrfx_nfct_evt_t const * event) {
     LOG_DEBUG("[IRQ] ");
     if (event->evt_id == NRFX_NFCT_EVT_RX_FRAMEEND) {
         LOG_DEBUG("Received frame of size %lu\n", event->params.rx_frameend.rx_data.data_size);
         receive_event.super.handler = receive_handler;
         receive_event.size = event->params.rx_frameend.rx_data.data_size;
-        event_post(&event_queue, (event_t *) &receive_event);   
+        event_post(&event_queue, (event_t *) &receive_event);
     } else if (event->evt_id == NRFX_NFCT_EVT_TX_FRAMEEND) {
         LOG_DEBUG("End of transmission\n");
         end_of_transmission_event.handler = end_of_tx_handler;
         event_post(&event_queue, &end_of_transmission_event);
     } else if (event->evt_id == NRFX_NFCT_EVT_SELECTED) {
         LOG_DEBUG("Field has been selected\n");
-        select_event.handler = selected_handler;
+        select_event.handler = select_handler;
         event_post(&event_queue, &select_event);
     } else if (event->evt_id == NRFX_NFCT_EVT_TX_FRAMESTART) {
         LOG_DEBUG("Transmitting data\n");
@@ -302,7 +361,7 @@ void irq_event_handler(nrfx_nfct_evt_t const * event) {
                 LOG_DEBUG("ERROR: %X\n", event->params.error.reason);
             }
         }
-        
+
     } else if (event->evt_id == NRFX_NFCT_EVT_FIELD_DETECTED) {
         LOG_DEBUG("Field detected\n");
         field_detected_event.handler = field_detected_handler;
@@ -332,7 +391,7 @@ void initialize_t2t(nfc_t2t_t *_tag) {
                          NRFX_NFCT_EVT_RX_FRAMEEND    |
                          NRFX_NFCT_EVT_RX_FRAMESTART  |
                          NRFX_NFCT_EVT_TX_FRAMEEND    |
-                         NRFX_NFCT_EVT_TX_FRAMESTART 
+                         NRFX_NFCT_EVT_TX_FRAMESTART
                          /* NRF_NFCT_INT_RXERROR_MASK    |
                          NRF_NFCT_RX_FRAME_STATUS_CRC_MASK    |
                          NRF_NFCT_RX_FRAME_STATUS_PARITY_MASK |
@@ -349,21 +408,20 @@ void initialize_t2t(nfc_t2t_t *_tag) {
 
     LOG_DEBUG("Initialization of NFCT driver successful!\n");
 
-    // this can be extracted from the t2t struct
-    uint8_t nfcid1[] = {0x04, 0x01, 0x02, 0x03};
-    uint8_t nfcid1_size = 4;
-    configure_autocolres(nfcid1, nfcid1_size);
-    /*
-    } else {
-        nrfx_nfct_autocolres_disable();
-    }
-    */
 
-    nrf_nfct_shorts_enable(NRF_NFCT, NRF_NFCT_SHORT_FIELDDETECTED_ACTIVATE_MASK | NRF_NFCT_SHORT_FIELDLOST_SENSE_MASK);
+    /* the nfcid is part of the tag */
+    uint8_t *nfcid1 = tag->sn.sn;
+    uint8_t nfcid1_size = tag->sn.sn_length;
+    configure_autocolres(nfcid1, nfcid1_size);
+
+    nrf_nfct_shorts_enable(NRF_NFCT, NRF_NFCT_SHORT_FIELDDETECTED_ACTIVATE_MASK |
+    NRF_NFCT_SHORT_FIELDLOST_SENSE_MASK);
 
     if (thread_pid == 0) {
         LOG_DEBUG("Creating thread for NFCT Type 2 Tag Emulator\n");
-        thread_pid = thread_create(thread_stack, sizeof(thread_stack), THREAD_PRIORITY_MAIN - 1, 0, nrfx_event_loop, NULL, "NRFX NFCT Type 2 Tag Emulator Thread");
+        thread_pid = thread_create(thread_stack, sizeof(thread_stack), THREAD_PRIORITY_MAIN - 1,
+                                   0, nrfx_event_loop, NULL,
+                                   "NRFX NFCT Type 2 Tag Emulator Thread");
     } else {
         LOG_WARNING("Thread already created, waking up thread\n");
         thread_wakeup(thread_pid);
